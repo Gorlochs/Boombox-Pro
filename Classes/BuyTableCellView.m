f4b988b9d7bc376995c4755ed0631374896f5a52
shawnb <shawnb@d2f66393-cc53-0410-8a13-95d1a00f47c2>
2008-11-14 06:16:40 +0000

    have custom cell on the Buy page displaying the correct price
    git-svn-id: http://literalshore.com/svn/gorlach/boombox/trunk@367 d2f66393-cc53-0410-8a13-95d1a00f47c2

--- Classes/BuyTableCellView.m
+++ Classes/BuyTableCellView.m
@@ -29,9 +29,11 @@
 	artistLabel.text = [songInfo objectForKey:@"artistName"];
 	songTitleLabel.text = [songInfo objectForKey:@"trackName"];
 	albumLabel.text = [songInfo objectForKey:@"collectionName"];
-	priceLabel.text = [NSString stringWithFormat:@"%d", [songInfo objectForKey:@"trackPrice"]];
+	priceLabel.text = [[NSString stringWithFormat:@"$%f", [[songInfo objectForKey:@"trackPrice"] floatValue]] stringByTrimmingCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"0"]];
+	
 	NSLog(@"trackPrice class: %@", [[songInfo objectForKey:@"trackPrice"] class]);
-	NSLog(@"trackPrice: %d", [[songInfo objectForKey:@"collectionPrice"] floatValue]);
+	NSLog(@"trackPrice: %f", [[songInfo objectForKey:@"trackPrice"] floatValue]);
+	NSLog(@"songInfo: %@", songInfo);
 }
 
 
