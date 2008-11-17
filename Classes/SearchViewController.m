4d4bc2c54139fb707bd9f912b0f8be8e2fa6e23e
shawnb <shawnb@d2f66393-cc53-0410-8a13-95d1a00f47c2>
2008-11-17 20:46:43 +0000

    memory cleanup
    git-svn-id: http://literalshore.com/svn/gorlach/boombox/trunk@422 d2f66393-cc53-0410-8a13-95d1a00f47c2

--- Classes/SearchViewController.m
+++ Classes/SearchViewController.m
@@ -45,6 +45,9 @@
 	[theTableView release];
 	[blipSearchBar release];
 	[adMobAd release];
+	[buySongListController release];
+	[searchCell release];
+	
 	
 	[rssParser release];
 	//[songs release];
