876fe9aece48ba3aab75c75f1f90ed81759e2d21
shawnb <shawnb@d2f66393-cc53-0410-8a13-95d1a00f47c2>
2009-01-19 20:30:29 +0000

    new class
    git-svn-id: http://literalshore.com/svn/gorlach/boombox/trunk@488 d2f66393-cc53-0410-8a13-95d1a00f47c2

---
+++ Classes/TopButtonView.h
@@ -0,0 +1,16 @@
+//
+//  TopButtonView.h
+//  iPhoneStreamingPlayer
+//
+//  Created by Shawn Bernard on 1/19/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface TopButtonView : UIView {
+
+}
+
+@end
---
+++ Classes/TopButtonView.m
@@ -0,0 +1,33 @@
+//
+//  TopButtonView.m
+//  iPhoneStreamingPlayer
+//
+//  Created by Shawn Bernard on 1/19/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import "TopButtonView.h"
+
+
+@implementation TopButtonView
+
+
+- (id)initWithFrame:(CGRect)frame {
+    if (self = [super initWithFrame:frame]) {
+        // Initialization code
+    }
+    return self;
+}
+
+
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+
+
+- (void)dealloc {
+    [super dealloc];
+}
+
+
+@end
